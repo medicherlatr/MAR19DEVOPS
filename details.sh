1a033035413a16b5920e894e07d0c95b18471608
testuser1 <testuser1@ip-10-50-1-244.ec2.internal>
2019-05-28 16:55:15 +0000

    adding few desciptions & users

--- details.sh
+++ details.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 echo “This script will provide information about the local system”
-echo hostname
+
+#Gives hostname
+echo $(hostname)
+
+#Gives user executed this Script
 echo $(whoami)
-echo $(id -un)
+
+#Gives Users in the System
+USERS=$(cat /etc/passwd | cut -d ":" -f 1)
+echo $USERS
 
